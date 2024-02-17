b30292610c5bc0f0fe7e14d3af6c5edb5ada9885
b1becker <128233074+b1becker@users.noreply.github.com>
2024-02-17 15:06:30 -0500

    webstuff

--- src/routes/chat/Chat.svelte
+++ src/routes/chat/Chat.svelte
@@ -11,13 +11,21 @@
 </head>
 <nav class = "header">
 	<div class = "header_container">
+		
+		
 		<a href="#home" id="our_logo" >Global Chat</a>
-
+		
+		<div class="navbar__toggle" id="mobile-menu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+		
 		<div class = "header_toggle" id = "mobile-menu">
-			<li class="navbar__item">
+			<li class="navbar_item">
                 <a href="#about" class="navbar_links" id="profile-page">About</a>
             </li>
-            <li class="navbar__item">
+            <li class="navbar_item">
                 <a href="#signup" class="navbar_links" id="career-page">Sign Up</a>
             </li>
 
